74731242a32f73d7d7c5c8d3b95e05fa74574f0d
hk <93450198+206CDE@users.noreply.github.com>
2021-11-13 03:28:35 +0800

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,1 +1,15 @@
-print("hello")
+import webbrowser
+import datetime
+import pymysql
+from flask import Flask, render_template, redirect, url_for, request
+
+
+
+#Connect to the database
+DB = pymysql.connect( 
+  host = "127.0.0.1",
+  user = "",
+  password = "",  
+  database = ""
+)
+CUR = DB.cursor() 
