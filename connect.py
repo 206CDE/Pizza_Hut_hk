baa86348fe039aea80f9edbd5f01edaa4feff28f
hk <93450198+206CDE@users.noreply.github.com>
2021-11-13 20:07:06 +0800

    Update connect.py

--- connect.py
+++ connect.py
@@ -7,8 +7,9 @@ from flask import Flask, render_template, redirect, url_for, request
 
 #Connect to the database
 DB = pymysql.connect( 
-  host = "sg2plzcpnl487151.prod.sin2.secureserver.net",
-  user = "cpses_xsjc4cya10",
+  host = "localhost",
+  user = "i8296166_wp1",
+  password = "Z.77XCsx57LUBE2vK9X68"
 )
 CUR = DB.cursor() 
 
