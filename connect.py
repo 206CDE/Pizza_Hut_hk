a365136e5e28c6d8e9075e15a85600aeb2e7adb1
hk <93450198+206CDE@users.noreply.github.com>
2021-11-13 19:57:10 +0800

    Update connect.py

--- connect.py
+++ connect.py
@@ -8,7 +8,7 @@ from flask import Flask, render_template, redirect, url_for, request
 #Connect to the database
 DB = pymysql.connect( 
   host = "sg2plzcpnl487151.prod.sin2.secureserver.net",
-  user = "cpses_xsjc4cya10@localhost",
+  user = "cpses_xsjc4cya10",
 )
 CUR = DB.cursor() 
 
