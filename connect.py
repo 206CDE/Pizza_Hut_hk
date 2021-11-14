de413abde0337d18c4d81fb8dc7578eb40b43c43
hk <93450198+206CDE@users.noreply.github.com>
2021-11-14 18:44:16 +0800

    Update connect.py

--- connect.py
+++ connect.py
@@ -109,6 +109,6 @@ def send_order():
               </body></html>"""
 
 if __name__=='__main__':
-  app.run(host="0.0.0.0", port=8000)
+  app.run(host="localhost", port=8000, debug = True)
 
 DB.close()
